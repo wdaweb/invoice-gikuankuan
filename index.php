573f542a34344d13417182f72e906bc114153703
kuan <a831202a@gmail.com>
2020-05-08 16:06:46 +0800

    新增成功跳回index，以及常是抓最後一筆ID代表發票的數量也要看一下合併計算的功能

--- index.php
+++ index.php
@@ -1,14 +1,48 @@
 <?php include_once "connect.php";?>
+<!-- include PDO 很方便 -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
+    <!-- 外部連結css樣式 -->
     <title>統一發票管理系統</title>
 
 </head>
 <body>
 <?php include "./layout/header.php";?>  
+
+
+<form action="save_invoice.php" method="post">
+    期別：
+        <select name="period">
+        <option value="1">1-2月</option>
+        <option value="2">3-4月</option>
+        <option value="3">5-6月</option>
+        <option value="4">7-8月</option>
+        <option value="5">9-10月</option>
+        <option value="6">11-12</option>
+        </select>
+    年份：
+    <select name="year">
+        <option value="1">2020</option>
+        <option value="2">2021</option>
+        <option value="3">2022</option>
+        </select>
+        <br>
+    獎號：
+    <input type="text" name="code">
+    <input type="number" name="number">
+    <br>
+    花費：
+    <input type="number" name="expend">
+    <input type="submit" value="儲存">
+    <br>
+    
+    <!-- 共有 <?=$m?>張發票
+    抓取最後一筆ID作為發票的總數 -->
+</form>
 </body>
 </html>
---
+++ save_invoice.php
@@ -0,0 +1,35 @@
+<?php
+include_once "connect.php";
+
+$sql="insert into invoice 
+(`period`,
+`year`,
+`code`,
+`number`,
+`expend`)
+values(
+    '".$_POST['period']."',
+    '".$_POST['year']."',
+    '".$_POST['code']."',
+    '".$_POST['number']."',
+    '".$_POST['expend']."')";
+
+
+    echo $sql;
+// echo "period=" .$_POST['period']."br";
+// echo "year=" .$_POST['year']."br";
+// echo "code=" .$_POST['code']."br";
+// echo "number=" .$_POST['number']."br";
+// echo "expend=" .$_POST['expend']."br";
+
+$res=$pdo -> exec($sql);
+
+if($res==1){
+ echo "新增成功";
+ header('location:http://localhost/invoice/');
+//  新增成功跳到首頁
+}else{
+    echo "新增失敗";
+}
+
+?>
